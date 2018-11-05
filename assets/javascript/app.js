4f598bb74c49257badd8b6bee913df7bbc861dc2
DAGMAX83 <DAGMAX983@gmail.com>
2018-11-05 09:27:32 -0600

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,13 @@ const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
 function buildQuiz(){
-    buildQuiz();
+    const output=[];
+    myQuestions.forEach(
+        (currentQuestion, QuestionNumber) =>{
+            cconst answers = [];
+            
+        }
+    )
 }
 
 function showResults(){
@@ -95,7 +101,46 @@ const myQuestions = [
     correctAnswer: "c"
   },
 
+function showResults(){
 
+// gather answer containers from our quiz
+const answerContainers = quizContainer.querySelectorAll('.answers');
+
+// keep track of user's answers
+let numCorrect = 0;
+
+// for each question...
+myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+  // find selected answer
+  const answerContainer = answerContainers[questionNumber];
+  const selector = 'input[name=question'+questionNumber+']:checked';
+  const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+  // if answer is correct
+  if(userAnswer===currentQuestion.correctAnswer){
+    // add to the number of correct answers
+    numCorrect++;
+
+    // color the answers green
+    answerContainers[questionNumber].style.color = 'lightgreen';
+  }
+  // if answer is wrong or blank
+  else{
+    // color the answers red
+    answerContainers[questionNumber].style.color = 'red';
+  }
+});
+
+// show number of correct answers out of total
+resultsContainer.innerHTML = numCorrect + ' out 
+
+function timesUp()
+{
+  score = calculateScore()
+  location = "results.html?score=" + score
+}
+setTimeout("timesUp()",60000)
 
 
 </script>
